eedd659edaf95da62ac53c9732be2ca32e938291
ethan <42422230+coolguyethanw@users.noreply.github.com>
2019-02-01 11:38:12 -1000

    Add files via upload

--- main.js
+++ main.js
@@ -134,66 +134,6 @@ var Room7Full = false;
 var Room8Full = true; //starts true for testing and because ice cream machine broke
 
 // ============= loading in the json items as variables and then inserting in page
- Menu1Name = mydata.menu[0].name;
-$.getElementById("fooditem1") = Menu1Name;
- Menu1Price = mydata.menu[0].price;
-$.getElementById("foodPrice1") = Menu1Price;
- Menu1Desc = mydata.menu[0].desc;
-$.getElementById("foodDesc1") = Menu1Desc;
-//// TODO: finish this shit with the descriptions
-
- Menu2Name = mydata.menu[1].name;
-$.getElementById("fooditem2") = Menu2Name;
- Menu2Price = mydata.menu[1].price;
-$.getElementById("foodPrice2") = Menu2Price;
- Menu2Desc = mydata.menu[1].desc;
-$.getElementById("foodDesc2") = Menu2Desc;
-
- Menu3Name = mydata.menu[2].name;
-$.getElementById("fooditem3") = Menu3Name;
- Menu3Price = mydata.menu[2].price;
-$.getElementById("foodPrice3") = Menu3Price;
- Menu3Desc = mydata.menu[2].desc;
-$.getElementById("foodDesc3") = Menu3Desc;
-
- Menu4Name = mydata.menu[3].name;
-$.getElementById("fooditem4") = Menu4Name;
- Menu4Price = mydata.menu[3].price;
-$.getElementById("foodPrice4") = Menu4Price;
- Menu4Desc = mydata.menu[3].desc;
-$.getElementById("foodDesc4") = Menu4Desc;
-
- Menu5Name = mydata.menu[4].name;
-$.getElementById("fooditem5") = Menu5Name;
- Menu5Price = mydata.menu[4].price;
-$.getElementById("foodPrice5") = Menu5Price;
- Menu5Desc = mydata.menu[4].desc;
-$.getElementById("foodDesc5") = Menu5Desc;
-
-
- Menu6Name = mydata.menu[5].name;
-$.getElementById("fooditem6") = Menu6Name;
- Menu6Price = mydata.menu[5].price;
-$.getElementById("foodPrice6") = Menu6Price;
- Menu6Desc = mydata.menu[5].desc;
-$.getElementById("foodDesc6") = Menu6Desc;
-
-
- Menu7Name = mydata.menu[6].name;
-$.getElementById("fooditem7") = Menu7Name;
- Menu7Price = mydata.menu[6].price;
-$.getElementById("foodPrice7") = Menu7Price;
- Menu7Desc = mydata.menu[6].desc;
-$.getElementById("foodDesc7") = Menu7Desc;
-
-
- Menu8Name = mydata.menu[7].name;
-$.getElementById("fooditem8") = Menu8Name;
- Menu8Price = mydata.menu[7].price;
-$.getElementById("foodPrice8") = Menu8Price;
- Menu8Desc = mydata.menu[7].desc;
-$.getElementById("foodDesc8") = Menu8Desc;
-
 
 /* var RoomClean1 = false;
 var RoomClean2 = false;
@@ -218,8 +158,8 @@ var RoomClean8 = false;
 var RoomClean1 = false;
 // list of check in foods
 function addfoodrm1() {
+  var food = Menu1Name
   NumfoodsRm1 += 1;
-  var food = document.getElementById('foodName1').value;
   //foodsRm1.push(food);
   // add food name to food list
   document.getElementById('statusbar1').innerHTML = "Enter food name!";
@@ -341,7 +281,8 @@ function clearRoom1() {
   ConsoleLog()
   while (num <= badfood) {
     if (NumfoodsRm1 >= 5) {
-      document.getElementById('statusbar1').innerHTML = "This ain't an all you-can-eat buffet";
+      var food = Menu1Name;
+      document.getElementById('statusbar1').innerHTML = "This isn't catering!!";
       // tell other funcs that room is full
       Room1Full = true;
       document.getElementById('button1', 'foodName1', 'buttonCLR1').disabled = true;
@@ -349,7 +290,7 @@ function clearRoom1() {
     }
       // TODO: find a way to stop people from overflowing the room effectively
     var food = Menu1Name
-    //foodsRm1.push(food);
+    foodsRm1.push(food);
     // add food name to food list
     var room = document.getElementById("GRoom1");
     // find the room that were about to thro w foods into
